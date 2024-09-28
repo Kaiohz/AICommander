dce75f726b0c745b75224e6cf34269c29a938b82
Kaiohz <goncalves.yohan@gmail.com>
2024-09-28 13:33:47 +0200

    better with read

--- commander.sh
+++ commander.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-USER_REQUEST="$1"
+# Prompt the user for input
+echo "Describe your command :"
+read USER_REQUEST
 
+# # Prepare the request body (adjust role and text as needed)
 REQUEST_BODY=$(cat <<EOF
 {
   "contents": [
@@ -19,8 +22,8 @@ REQUEST_BODY=$(cat <<EOF
     "parts": [
       {
         "text": "you are an ai specialized in unix command. you will 
-receive a user prompt and give the unix command related to the user 
-request. you have to only answer with command."
+receive a user request and give the unix command related to the user 
+request. Answer only with the command."
       }
     ]
   },
@@ -35,6 +38,7 @@ request. you have to only answer with command."
 EOF
 )
 
+# Send the request and capture the response
 RESPONSE=$(curl -sX POST https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${AI_COMMANDER_API_KEY} -H 'Content-Type: application/json' -d "$REQUEST_BODY")
 
 COMMAND=$(jq -r '.candidates[0].content.parts[0].text' <<< "$RESPONSE")
